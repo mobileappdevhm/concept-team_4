2300cef4906776c2e3703309dfd5c20f253aba4e
Gleydar <gleydar@outlook.de>
2018-04-23 14:11:47 +0200

    Added Top and Bottom Spacing

--- lib/views/MainView.dart
+++ lib/views/MainView.dart
@@ -11,7 +11,9 @@ class _MainViewState extends State<MainView> {
   Widget build(BuildContext context) {
     return new Center(
         child: new Column(
+          
       children: <Widget>[
+        new Container(height: 20.0,),
         new Expanded(
             child: new Row(
           children: <Widget>[
@@ -22,17 +24,15 @@ class _MainViewState extends State<MainView> {
                   border: new Border(
                     top: const BorderSide(width: 3.0, color: Colors.redAccent),
                     left: const BorderSide(width: 3.0, color: Colors.redAccent),
-                    right:
-                        const BorderSide(width: 3.0, color: Colors.redAccent),
-                    bottom:
-                        const BorderSide(width: 3.0, color: Colors.redAccent),
+                    right: const BorderSide(width: 3.0, color: Colors.redAccent),
+                    bottom: const BorderSide(width: 3.0, color: Colors.redAccent),
                   )),
               child: new GestureDetector(
                 child: new Column(
                   children: <Widget>[
                     new Center(
                         child: const Icon(
-                      Icons.ac_unit,
+                      Icons.school,
                       size: 70.0,
                     )),
                     new Text(
@@ -45,23 +45,178 @@ class _MainViewState extends State<MainView> {
                 ),
               ),
             )),
-            new Expanded(child: new Placeholder())
+            new Expanded(
+                child: new Row(
+                  children: <Widget>[
+                    new Expanded(
+                        child: new Container(
+                          decoration: new BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                              border: new Border(
+                                top: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                left: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                right: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                bottom: const BorderSide(width: 3.0, color: Colors.redAccent),
+                              )),
+                          child: new GestureDetector(
+                            child: new Column(
+                              children: <Widget>[
+                                new Center(
+                                    child: const Icon(
+                                      Icons.star,
+                                      size: 70.0,
+                                    )),
+                                new Text(
+                                  'FAVORITES',
+                                  style: new TextStyle(fontSize: 30.0),
+                                )
+                              ],
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                            ),
+                          ),
+                        )),
+                  ],
+                ))
           ],
-        )),
+        )), //Row 1
+
+
         new Expanded(
             child: new Row(
-          children: <Widget>[
-            new Expanded(child: new Placeholder()),
-            new Expanded(child: new Placeholder())
-          ],
-        )),
+              children: <Widget>[
+                new Expanded(
+                    child: new Container(
+                      decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          border: new Border(
+                            top: const BorderSide(width: 3.0, color: Colors.redAccent),
+                            left: const BorderSide(width: 3.0, color: Colors.redAccent),
+                            right: const BorderSide(width: 3.0, color: Colors.redAccent),
+                            bottom: const BorderSide(width: 3.0, color: Colors.redAccent),
+                          )),
+                      child: new GestureDetector(
+                        child: new Column(
+                          children: <Widget>[
+                            new Center(
+                                child: const Icon(
+                                  Icons.access_time,
+                                  size: 70.0,
+                                )),
+                            new Text(
+                              'SCHEDULE',
+                              style: new TextStyle(fontSize: 30.0),
+                            )
+                          ],
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ),
+                      ),
+                    )),
+                new Expanded(
+                    child: new Row(
+                      children: <Widget>[
+                        new Expanded(
+                            child: new Container(
+                              decoration: new BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  border: new Border(
+                                    top: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                    left: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                    right: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                    bottom: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                  )),
+                              child: new GestureDetector(
+                                child: new Column(
+                                  children: <Widget>[
+                                    new Center(
+                                        child: const Icon(
+                                          Icons.forum,
+                                          size: 70.0,
+                                        )),
+                                    new Text(
+                                      'FORUM',
+                                      style: new TextStyle(fontSize: 30.0),
+                                    )
+                                  ],
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                ),
+                              ),
+                            )),
+                      ],
+                    ))
+              ],
+            )), // Row 2
+
+
         new Expanded(
             child: new Row(
-          children: <Widget>[
-            new Expanded(child: new Placeholder()),
-            new Expanded(child: new Placeholder())
-          ],
-        ))
+              children: <Widget>[
+                new Expanded(
+                    child: new Container(
+                      decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                          border: new Border(
+                            top: const BorderSide(width: 3.0, color: Colors.redAccent),
+                            left: const BorderSide(width: 3.0, color: Colors.redAccent),
+                            right: const BorderSide(width: 3.0, color: Colors.redAccent),
+                            bottom: const BorderSide(width: 3.0, color: Colors.redAccent),
+                          )),
+                      child: new GestureDetector(
+                        child: new Column(
+                          children: <Widget>[
+                            new Center(
+                                child: const Icon(
+                                  Icons.contact_mail,
+                                  size: 70.0,
+                                )),
+                            new Text(
+                              'CONTACT',
+                              style: new TextStyle(fontSize: 30.0),
+                            )
+                          ],
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ),
+                      ),
+                    )),
+                new Expanded(
+                    child: new Row(
+                      children: <Widget>[
+                        new Expanded(
+                            child: new Container(
+                              decoration: new BoxDecoration(
+                                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                                  border: new Border(
+                                    top: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                    left: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                    right: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                    bottom: const BorderSide(width: 3.0, color: Colors.redAccent),
+                                  )),
+                              child: new GestureDetector(
+                                child: new Column(
+                                  children: <Widget>[
+                                    new Center(
+                                        child: const Icon(
+                                          Icons.explore,
+                                          size: 70.0,
+                                        )),
+                                    new Text(
+                                      'LOCATIONS',
+                                      style: new TextStyle(fontSize: 30.0),
+                                    )
+                                  ],
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                ),
+                              ),
+                            )),
+                      ],
+                    ))
+              ],
+            )),
+        new Container(height: 20.0,),
       ],
     ));
   }
