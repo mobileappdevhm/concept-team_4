c91b9d105b031444f9dd6d16ab65393c7db27345
Gleydar <gleydar@outlook.de>
2018-04-23 14:00:15 +0200

    Vertical Prototype fixed

--- lib/views/MainView.dart
+++ lib/views/MainView.dart
@@ -14,18 +14,55 @@ class _MainViewState extends State<MainView> {
       children: <Widget>[
         new Expanded(
             child: new Row(
-          children: <Widget>[ new Expanded(child: new Column(children: <Widget>[new Center(child: const Icon(Icons.ac_unit, size: 70.0,)), new Text('COURSES', style: new TextStyle(fontSize: 30.0),)], crossAxisAlignment: CrossAxisAlignment.center, mainAxisAlignment: MainAxisAlignment.center,),), new Expanded(child: new Placeholder())],
+          children: <Widget>[
+            new Expanded(
+                child: new Container(
+              decoration: new BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  border: new Border(
+                    top: const BorderSide(width: 3.0, color: Colors.redAccent),
+                    left: const BorderSide(width: 3.0, color: Colors.redAccent),
+                    right:
+                        const BorderSide(width: 3.0, color: Colors.redAccent),
+                    bottom:
+                        const BorderSide(width: 3.0, color: Colors.redAccent),
+                  )),
+              child: new GestureDetector(
+                child: new Column(
+                  children: <Widget>[
+                    new Center(
+                        child: const Icon(
+                      Icons.ac_unit,
+                      size: 70.0,
+                    )),
+                    new Text(
+                      'COURSES',
+                      style: new TextStyle(fontSize: 30.0),
+                    )
+                  ],
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+              ),
+            )),
+            new Expanded(child: new Placeholder())
+          ],
         )),
         new Expanded(
             child: new Row(
-          children: <Widget>[new Expanded(child: new Placeholder()), new Expanded(child: new Placeholder())],
+          children: <Widget>[
+            new Expanded(child: new Placeholder()),
+            new Expanded(child: new Placeholder())
+          ],
         )),
         new Expanded(
             child: new Row(
-              children: <Widget>[new Expanded(child: new Placeholder()), new Expanded(child: new Placeholder())],
-            ))
+          children: <Widget>[
+            new Expanded(child: new Placeholder()),
+            new Expanded(child: new Placeholder())
+          ],
+        ))
       ],
     ));
   }
-
 }
